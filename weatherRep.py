01a074d04f4e47b801472cdbd1206bd0c63adeaa
februarysea <31513558+februarysea@users.noreply.github.com>
2018-10-24 19:41:19 +0800

    Update weatherRep.py

--- weatherRep.py
+++ weatherRep.py
@@ -1,36 +1,33 @@
 from weather import Weather, Unit
 from twilio.rest import Client
+from apscheduler.schedulers.blocking import BlockingScheduler
 
-import weatherCode
+def weather():
+    # get weather infrmation
+    weather = Weather(unit=Unit.CELSIUS)
+    location = weather.lookup_by_location('shanghai')
+    condition = location.condition
+    forecasts = location.forecast
 
-weather = Weather(unit=Unit.CELSIUS)
-location = weather.lookup_by_location('shanghai')
-condition = location.condition
-print(condition.text)
-forecasts = location.forecast
-print(forecasts[0].text)
-print(forecasts[0].date)
-print(forecasts[0].high)
-print(forecasts[0].low)
+    text = 'Good morning! Here is weather report from februarysea! ' + 'Today is ' \
+           + forecasts[0].date + '. ' + 'Now the weather is ' \
+           + condition.text + '. ' + 'The highest temperature is ' \
+           + forecasts[0].high + '. ' + 'The lowest temperature is ' \
+           + forecasts[0].low + '. ' + 'Today the forecast is ' \
+           + forecasts[0].text + '. ' + 'Have a nice day! :)'
+    print(text)
 
-text = 'Good morning! Here is weather report from februarysea! ' + 'Today is '\
-       + forecasts[0].date + '. ' + 'The high temperature is '\
-       + forecasts[0].high + '. ' + 'The low temperature is '\
-       + forecasts[0].low + '. ' + 'The forecast is '\
-       + forecasts[0].text + '. ' + 'Have a nice day! :)'
+    # send message
+    account_sid = '' #here is your account_sid from twilio
+    auth_token = '' #here is your auth_token from twilio
+    client = Client(account_sid, auth_token)
 
-print(text)
+    message = client.messages.create(
+        from_='', #here is your phonenumber get from twilio
+        body=text,
+        to='' #here is your phonenumber when you sign in writing
+    )
 
-
-
-#send message
-account_sid = '#'
-auth_token = '#'
-client = Client(account_sid, auth_token)
-
-message = client.messages.create(
-    from_='#',
-    body=text,
-    to='#'
-)
-print(message.sid)
+scheduler = BlockingScheduler()
+scheduler.add_job(weather, 'cron', day_of_week='0-6', hour=7, minute=00)
+scheduler.start()
